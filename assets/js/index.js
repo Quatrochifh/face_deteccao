d873fca471e3e80f3092d95697b3486aaf3a24d5
Fábio Henrique Quatrochi <39220866838@info.scs>
2022-02-02 14:22:22 -0300

    camera resolvido

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
-navigator.mediaDevices.getUserMedia({ video:true })
+// navigator.mediaDevices.getUserMedia({ video:true })
+
+document.querySelector('button').addEventListener('click', async (e) => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true
+      })
+      document.querySelector('video').srcObject = stream
+    } catch(e) {
+      console.error(e)
+    }
+  })
